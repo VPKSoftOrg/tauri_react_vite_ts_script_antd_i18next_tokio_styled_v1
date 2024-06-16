90224f7c887a4446da4bd798852c090534d87125
Petteri Kautonen <vpksoft@vpksoft.net>
2024-06-16 12:57:46 +0300

    Fix the endless save window state.

--- src/hooks/UseWindowStateListener.tsx
+++ src/hooks/UseWindowStateListener.tsx
@@ -91,7 +91,7 @@ const useWindowStateSaver = (intervalMs: number) => {
             }
 
             // Save the window state if the time interval has passed, the window event has occurred and the state saver is enabled.
-            if (stateSaverEnabled && windowEventOccurred.current === true) {
+            if (stateSaverEnabled && windowEventOccurred.current === true && intervalPassed.current === true) {
                 windowEventOccurred.current = false;
                 void saveWindowState(StateFlags.ALL);
                 intervalPassed.current = false;
